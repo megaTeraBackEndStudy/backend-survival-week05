fbe82a862c0c786e9d2376900d5a262fd8709c66
황경훈 <rudgns98@hdene.co.kr>
2024-04-19 19:04:55 +0900

    과제

--- src/main/java/kr/megaptera/assignment/application/DeleteCommentService.java
+++ src/main/java/kr/megaptera/assignment/application/DeleteCommentService.java
@@ -1,4 +1,15 @@
 package kr.megaptera.assignment.application;
 
+import kr.megaptera.assignment.dtos.CommentResponseDto;
+import kr.megaptera.assignment.repository.CommentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class DeleteCommentService {
+  private final CommentRepository commentRepository;
+  public CommentResponseDto deleteComment(String commentId) {
+    return CommentResponseDto.of(commentRepository.delete(commentId));
+  }
 }
--- src/main/java/kr/megaptera/assignment/controllers/CommentController.java
+++ src/main/java/kr/megaptera/assignment/controllers/CommentController.java
@@ -1,4 +1,57 @@
 package kr.megaptera.assignment.controllers;
 
+import kr.megaptera.assignment.application.CreateCommentService;
+import kr.megaptera.assignment.application.DeleteCommentService;
+import kr.megaptera.assignment.application.GetCommentsService;
+import kr.megaptera.assignment.application.UpdateCommentService;
+import kr.megaptera.assignment.dtos.CommentCreateRequestDto;
+import kr.megaptera.assignment.dtos.CommentResponseDto;
+import kr.megaptera.assignment.dtos.CommentUpdateRequestDto;
+import kr.megaptera.assignment.exceptions.CommentNotFound;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/comments")
+@RequiredArgsConstructor
 public class CommentController {
+  private final GetCommentsService getCommentsService;
+  private final CreateCommentService createCommentService;
+  private final UpdateCommentService updateCommentService;
+  private final DeleteCommentService deleteCommentService;
+  @GetMapping
+  public List<CommentResponseDto> detail(@RequestParam String postId) {
+    return getCommentsService.getCommentList(postId);
+  }
+
+  @PostMapping
+  @ResponseStatus(HttpStatus.CREATED)
+  public CommentResponseDto create(@RequestParam String postId,
+      @RequestBody CommentCreateRequestDto request) {
+    request.setPostId(postId);
+    return createCommentService.createComment(request);
+
+  }
+
+  @PatchMapping("/{id}")
+  public CommentResponseDto update(@PathVariable String id,
+      @RequestBody CommentUpdateRequestDto commentUpdateDto) {
+    return updateCommentService.update(id, commentUpdateDto);
+
+  }
+
+  @DeleteMapping("/{id}")
+  public CommentResponseDto delete(@PathVariable String id) {
+    return deleteCommentService.deleteComment(id);
+  }
+
+
+  @ExceptionHandler(CommentNotFound.class)
+  @ResponseStatus(HttpStatus.NOT_FOUND)
+  public String postNotFound() {
+    return "댓글을 찾을 수 없습니다.";
+  }
 }
